0c15a98f27b4d7a4465efbb112b85cf911f6aaea
Bogacz, Michal <michal.bogacz@sap.com>
2017-10-17 12:56:14 +0200

    added solution for task dht + led

---
+++ step2/dht-with-led/dht-with-led.ino
@@ -0,0 +1,37 @@
+#include <DHT.h>
+
+#define DHTPIN            D4         // Pin which is connected to the DHT sensor.
+#define DHTTYPE           DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+uint32_t delayMS = 10000;
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin(); // Initialize device.
+  Serial.println("DHT1 Example");
+
+  pinMode(BUILTIN_LED, OUTPUT);
+}
+
+void loop() {
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.println("%");
+
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.println(" *C");
+  
+  if(t<28.0){
+    digitalWrite(BUILTIN_LED, HIGH);      
+  }else{
+    digitalWrite(BUILTIN_LED, LOW); 
+  }
+
+  delay(delayMS);
+}
